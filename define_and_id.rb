49b45db130af06cd1497580fa3e3c0f08010b59a
Harrison Ryan <hwryan12@gmail.com>
2022-11-03 12:47:19 -0500

    Complete define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,25 @@
 
 
 "Hello World".include?("Hello")
-
+# We have the string object "Hello World" being called upon the include method with the argument "Hello" being passed.
+# Include's one function is to determine if the argument "Hello" set in the parameters is included in the string object "Hello World"
+# The return value is `true`
 
 "Hello World".end_with?("Hello")
-
+# The string object "Hello World" being called upon the end_with method with the argument "Hello" being passed.
+# End_with's one function is to determine if the argument "Hello" set in the parameters are the last characters in the string object "Hello World".
+# The return value is `false`
 
 "Hello World".end_with?("rld")
-
+# The string object "Hello World" being called upon the end_with method with the argument "rld" being passed.
+# End_with's one function is to determine if the argument "rld" set in the parameters are the last characters in the string object "Hello World".
+# The return value is `true`
 
 12.even?
-
+# The even? method is calling on the integer 12 to determine if it is an even integer
+# The result is true
 
 18.next
-
+# The next method is calling on the integer 18 to determine what the next, proceeding whole number after 18 is
+# The result is 19
 
