eeca07efad8d085d948a436bd0f7c1d97c1919b8
Harrison Ryan <hwryan12@gmail.com>
2022-11-03 14:12:16 -0500

    Add final_practice.rb

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,65 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+    "Hello, how are you?"
+  end
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
+# "Hello, how are you?"
 
+# How many arguments did you pass your method?
+# I did not pass any arguments.
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
-
+def greeting(name)
+    "Hey, #{name}!"
+  end
+greeting("Harrison")
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# "Hey, Harrison!"
 
+# How many arguments did you pass your method?
+# I passed the argument "Harrison".
+
+# What data type was your argument(s)?
+# "Harrison" is a string.
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
+def square(num)
+  Integer.sqrt(num)
+end
 
+p square(24)
 
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# The return value is 4.
 
+# How many arguments did you pass your method?
+# Passed one argument, `num`.
+
+# What data type was your argument(s)?
+# Integer
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
-
-
+def greet_person(first, mid, last)
+    "Howdy, #{first} #{mid} #{last}!"
+    
+  end
+ p greet_person("Harrison", "William", "Ryan")
 # What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+#--> "Howdy, Harrison WIlliam Ryan!"
 
+# How many arguments did you pass your method?
+# I passed three arguments, `Harrison`, `William`, and `Ryan`.
+
+# What data type was your argument(s)?
+# My data types were an array.
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,6 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+
 - How confident do you feel writing simple methods of your own?
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
